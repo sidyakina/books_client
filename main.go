9027c9e28c8f9fa2df132d4e15b579700a763b61
Sidykina Liza <liza_s95@mail.ru>
2019-02-14 19:11:56 +0300

    add some check and change diveder for parameter add because ; may be exist in name

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main (){
@@ -16,6 +17,14 @@ func main (){
 		text := scanner.Text()
 		cmd := strings.Split(text, " ")
 		switch cmd[0] {
+		case "help":
+			fmt.Println("cmds:")
+			fmt.Println("close - for exit (no parameters)")
+			fmt.Println("list - for view all books (no parameters)")
+			fmt.Println("add - for add book (parameters name, author, year)")
+			fmt.Println(" Example: add new book;;author;;1834")
+			fmt.Println("add - for remove book (parameter id)")
+			fmt.Println(" Example: remove 188809934")
 		case "close":
 			fmt.Println("cmd close")
 			return
@@ -24,21 +33,36 @@ func main (){
 			getAllBook()
 		case "add":
 			fmt.Println("cmd add")
-			params := strings.Split(text[4:], ";")
+			params := strings.Split(strings.Trim(text[3:], " "), ";;")
 			if len(params) != 3{
-				fmt.Printf("Invalid parameters!%v %v\n", len(params), params)
+				fmt.Printf("Invalid number parameters!\n")
 				break
 			}
 			year, err := strconv.ParseInt(strings.Trim(params[2], " "), 10, 0)
 			if err != nil {
-				fmt.Printf("Error while parse %v\n", err)
+				fmt.Printf("Error while parse: %v\n", err)
+				break
+			}
+			if int(year) > time.Now().Year() || year <= 0{
+				fmt.Printf("Invalid parameter for year:%v \n", year)
+				break
+			}
+			if params[0] == "" || params[1] == "" {
+				fmt.Printf("Invalid empty parameter \n")
+				break
 			}
 			addBook(params[0], params[1], int16(year))
 		case "remove":
 			fmt.Println("cmd remove")
-			id, err := strconv.ParseInt(strings.Trim(cmd[1], " "), 10, 0)
+			param := strings.Trim(text[6:], " ")
+			if len(param) == 0 {
+				fmt.Printf("Invalid empty parameter!\n")
+				break
+			}
+			id, err := strconv.ParseInt(param, 10, 0)
 			if err != nil {
-				fmt.Printf("Error while parse %v\n", err)
+				fmt.Printf("Error while parse: %v\n", err)
+				break
 			}
 			removeBook(int32(id))
 		default:
